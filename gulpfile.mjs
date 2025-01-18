5dcf35594a910bc7879db282ac9d590eaf5461f9
Gil Browdy <gil@pgedge.com>
2025-01-17 21:07:12 -0500

    WIP: First crucible compendium

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,6 +6,9 @@ import sourcemaps from "gulp-sourcemaps";
 import yaml from "gulp-yaml";
 import buffer from "vinyl-buffer";
 import source from "vinyl-source-stream";
+import path from 'path';
+import shell from 'gulp-shell';
+import mergeStream from 'merge-stream';
 
 import rollupStream from "@rollup/stream";
 
@@ -23,6 +26,56 @@ const stylesExtension = "scss";
 const sourceFileExtension = "mjs";
 const staticFiles = ["assets", "templates"];
 const systemYaml = ["src/**/*.{yml, yaml}"];
+
+// Constants.
+const PACK_SRC = `${sourceDirectory}/packs/src`;
+const PACK_DEST = `${distDirectory}/packs`;
+
+/* ----------------------------------------- */
+/*  Compile Compendia
+/* ----------------------------------------- */
+
+/**
+ * Gulp Task: Compile packs from the yaml source content to .db files.
+ */
+function compilePacks() {
+  // Every folder in the src dir will become a compendium.
+  const folders = fs.readdirSync(PACK_SRC).filter((file) => {
+    return fs.statSync(path.join(PACK_SRC, file)).isDirectory();
+  });
+
+  const packs = folders.map((folder) => {
+	console.log(folder);
+    return gulp.src(path.join(PACK_SRC, folder))
+      .pipe(shell([
+        `fvtt package --id grimwild --type System pack <%= file.stem %> -c --yaml --in "<%= file.path %>" --out ${PACK_DEST}`
+      ]))
+  })
+
+  return mergeStream.call(null, packs);
+}
+
+/* ----------------------------------------- */
+/*  Export Compendia
+/* ----------------------------------------- */
+
+/**
+ * Gulp Task: Export Packs
+ *
+ * This gulp task will load all compendium .db files from the dest directory,
+ * load them into memory, and then export them to a human-readable YAML format.
+ */
+function extractPacks() {
+  // Start a stream for all db files in the packs dir.
+  const packs = gulp.src(`${PACK_DEST}/*`)
+    .pipe(shell([
+      'fvtt package --id archmage --type System unpack <%= file.stem %> -c --yaml --in dist/packs --out src/packs/src/<%= file.stem %>'
+    ]));
+
+  // Call the streams.
+  return mergeStream.call(null, packs);
+}
+
 
 /** ******************/
 /*      BUILD       */
@@ -86,6 +139,7 @@ export function watch() {
 	gulp.watch(`${sourceDirectory}/**/*.{yml, yaml}`, { ignoreInitial: false }, buildYaml);
 	gulp.watch(`${sourceDirectory}/**/*.${sourceFileExtension}`, { ignoreInitial: false }, buildCode);
 	gulp.watch(`${stylesDirectory}/**/*.${stylesExtension}`, { ignoreInitial: false }, buildStyles);
+	gulp.watch(`${PACK_SRC}/**/*.{yml, yaml}`, { ignoreInitial: false }, compilePacks)
 	gulp.watch(
 		staticFiles.map((file) => `${sourceDirectory}/${file}`),
 		{ ignoreInitial: false },
@@ -93,7 +147,7 @@ export function watch() {
 	);
 }
 
-export const build = gulp.series(clean, gulp.parallel(buildYaml, buildCode, buildStyles, copyFiles));
+export const build = gulp.series(clean, gulp.parallel(compilePacks, buildYaml, buildCode, buildStyles, copyFiles));
 
 /** ******************/
 /*      CLEAN       */
@@ -103,7 +157,7 @@ export const build = gulp.series(clean, gulp.parallel(buildYaml, buildCode, buil
  * Remove built files from `dist` folder while ignoring source files
  */
 export async function clean() {
-	const files = [...staticFiles, "lang", "module", "system.json", "template.json"];
+	const files = [...staticFiles, "lang", "module", "packs", "system.json", "template.json"];
 
 	if (fs.existsSync(`${stylesDirectory}/src/${packageId}.${stylesExtension}`)) {
 		files.push("styles");
