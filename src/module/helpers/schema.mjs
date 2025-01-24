dff29a57847926243c644b35849b8076f1a261d8
Gil Browdy <gil@pgedge.com>
2025-01-24 07:23:32 -0500

    Create DicePoolField schema

---
+++ src/module/helpers/schema.mjs
@@ -0,0 +1,16 @@
+
+const fields = foundry.data.fields;
+const requiredInteger = { required: true, nullable: false, integer: true };
+
+export class DicePoolField extends fields.SchemaField {
+    constructor(options, context = {}) {
+        const dpFields = {
+            diceNum: new fields.NumberField({
+                ...requiredInteger,
+                initial: 1,
+                min: 0
+            })
+        };
+        super(dpFields, options, context);
+    }
+}
