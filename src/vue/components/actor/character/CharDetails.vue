821d62c6510076709337d2d6808e738d77e33858
Matt Smith <mattsmith142003@gmail.com>
2025-01-30 20:54:24 -0600

    Update layout of backgrounds

--- src/vue/components/actor/character/CharDetails.vue
+++ src/vue/components/actor/character/CharDetails.vue
@@ -1,42 +1,44 @@
 <template>
-	<fieldset>
-		<legend>{{ context.systemFields.backgrounds.label }}</legend>
-		<div class="background grid grid-3col" v-for="(background, key) in context.system.backgrounds" :key="key">
-			<div class="background-name form-group stacked grid-span-1">
-				<label>{{ game.i18n.localize('Name') }}</label>
-				<input type="text"
-					:name="`system.backgrounds.${key}.name`"
-					v-model="background.name"
-					:placeholder="game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.backgrounds.placeholder')"
-				/>
-			</div>
-			<div class="wises form-group stacked grid-span-2">
-				<label>{{ game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.backgrounds.FIELDS.wises.label') }}</label>
-				<div class="form-group">
+	<div class="grid grid-2col">
+		<fieldset>
+			<legend>{{ context.systemFields.backgrounds.label }}</legend>
+			<div class="background flexcol" v-for="(background, key) in context.system.backgrounds" :key="key">
+				<div class="background-name form-group stacked">
+					<label>{{ game.i18n.localize('Name') }}</label>
 					<input type="text"
-						:name="`system.backgrounds.${key}.wises.${0}`"
-						v-model="background.wises[0]"
-					/>
-					<input type="text"
-						:name="`system.backgrounds.${key}.wises.${1}`"
-						v-model="background.wises[1]"
-					/>
-					<input type="text"
-						:name="`system.backgrounds.${key}.wises.${2}`"
-						v-model="background.wises[2]"
+						:name="`system.backgrounds.${key}.name`"
+						v-model="background.name"
+						:placeholder="game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.backgrounds.placeholder')"
 					/>
 				</div>
+				<div class="wises form-group stacked">
+					<label>{{ game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.backgrounds.FIELDS.wises.label') }}</label>
+					<div class="form-group stacked">
+						<input type="text"
+							:name="`system.backgrounds.${key}.wises.${0}`"
+							v-model="background.wises[0]"
+						/>
+						<input type="text"
+							:name="`system.backgrounds.${key}.wises.${1}`"
+							v-model="background.wises[1]"
+						/>
+						<input type="text"
+							:name="`system.backgrounds.${key}.wises.${2}`"
+							v-model="background.wises[2]"
+						/>
+					</div>
+				</div>
 			</div>
-		</div>
-	</fieldset>
-	<fieldset>
-		<legend>{{ context.systemFields.biography.label }}</legend>
-		<div class="form-group">
-			<div class="field">
-				<Prosemirror :editable="context.editable" :field="context.editors['system.biography']"/>
+		</fieldset>
+		<fieldset>
+			<legend>{{ context.systemFields.biography.label }}</legend>
+			<div class="form-group">
+				<div class="field">
+					<Prosemirror :editable="context.editable" :field="context.editors['system.biography']"/>
+				</div>
 			</div>
-		</div>
-	</fieldset>
+		</fieldset>
+	</div>
 </template>
 
 <script setup>
--- src/vue/components/actor/character/CharSidebar.vue
+++ src/vue/components/actor/character/CharSidebar.vue
@@ -20,25 +20,29 @@
 				<label>{{ game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.path.label') }}</label>
 				<input type="text" name="system.path" v-model="context.actor.system.path"/>
 			</div>
-			<div class="form-group stacked">
+			<div class="traits form-group stacked">
 				<label>{{ context.systemFields.traits.label }}</label>
-				<div class="trait form-group" v-for="(trait, i) in context.system.traits" :key="i">
-					<input type="checkbox" :name="`system.traits.${i}.are`" v-model="trait.are"/>
-					<input type="text" :name="`system.traits.${i}.value`" v-model="trait.value" list="traits-list"/>
+				<div class="form-group stacked">
+					<div class="trait form-group" v-for="(trait, i) in context.system.traits" :key="i">
+						<input type="checkbox" :name="`system.traits.${i}.are`" v-model="trait.are"/>
+						<input type="text" :name="`system.traits.${i}.value`" v-model="trait.value" list="traits-list"/>
+					</div>
+					<datalist id="traits-list">
+						<option v-for="(trait, key) in CONFIG.GRIMWILD.traits" :key="key" :value="game.i18n.localize(trait)">{{ game.i18n.localize(trait) }}</option>
+					</datalist>
 				</div>
-				<datalist id="traits-list">
-					<option v-for="(trait, key) in CONFIG.GRIMWILD.traits" :key="key" :value="game.i18n.localize(trait)">{{ game.i18n.localize(trait) }}</option>
-				</datalist>
 			</div>
-			<div class="form-group stacked">
+			<div class="desires form-group stacked">
 				<label>{{ context.systemFields.desires.label }}</label>
-				<div class="desire form-group" v-for="(desire, i) in context.system.desires" :key="i">
-					<input type="checkbox" :name="`system.desires.${i}.are`" v-model="desire.are"/>
-					<input type="text" :name="`system.desires.${i}.value`" v-model="desire.value" list="desires-list"/>
+				<div class="form-group stacked">
+					<div class="desire form-group" v-for="(desire, i) in context.system.desires" :key="i">
+						<input type="checkbox" :name="`system.desires.${i}.are`" v-model="desire.are"/>
+						<input type="text" :name="`system.desires.${i}.value`" v-model="desire.value" list="desires-list"/>
+					</div>
+					<datalist id="desires-list">
+						<option v-for="(desire, key) in CONFIG.GRIMWILD.desires" :key="key" :value="game.i18n.localize(desire)">{{ game.i18n.localize(desire) }}</option>
+					</datalist>
 				</div>
-				<datalist id="desires-list">
-					<option v-for="(desire, key) in CONFIG.GRIMWILD.desires" :key="key" :value="game.i18n.localize(desire)">{{ game.i18n.localize(desire) }}</option>
-				</datalist>
 			</div>
 			<div class="form-group stacked">
 				<label>{{ game.i18n.localize('GRIMWILD.Actor.Character.FIELDS.xp.short') }}</label>
