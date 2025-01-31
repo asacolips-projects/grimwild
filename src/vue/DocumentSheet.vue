58f17c5db6c3d4e8d987ac2bd312215056246fc7
Matt Smith <mattsmith142003@gmail.com>
2025-01-31 11:54:21 -0600

    Add scroll regions CSS for body area

--- src/vue/DocumentSheet.vue
+++ src/vue/DocumentSheet.vue
@@ -1,17 +1,17 @@
 <template>
 	<div :class="`grimwild-vue standard-form flexcol`">
-		<div class="grid grid-4col">
+		<div class="grimwild-sheet-layout grid grid-4col">
 			<CharSidebar :context="context" />
 	
 			<!-- Header -->
-			 <section class="grimwild-main grid-span-3">
+			 <section class="grimwild-main flexcol grid-span-3">
 				<CharHeader :context="context" />
 				
-				<div class="section--main">
+				<div class="section--main flexcol">
+					<!-- Tab links -->
+					<Tabs :tabs="tabs.primary" no-span="true"/>
+
 					<section class="section--fields">
-						<!-- Tab links -->
-						<Tabs :tabs="tabs.primary" no-span="true"/>
-				
 						<!-- Details fields -->
 						<Tab group="primary" :tab="tabs.primary.details">
 							<CharDetails :actor="context.actor" :context="context"/>
