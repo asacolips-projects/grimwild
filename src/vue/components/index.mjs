4eec00da11792cfb9918cfb151a970fc66bab243
Gil Browdy <gil@pgedge.com>
2025-02-06 12:06:27 -0500

    Run npm lint:fix

--- src/vue/components/index.mjs
+++ src/vue/components/index.mjs
@@ -1,15 +1,15 @@
 // Character.
-export { default as CharSidebar } from '@/components/actor/character/CharSidebar.vue';
-export { default as CharHeader } from '@/components/actor/character/CharHeader.vue';
-export { default as CharDetails } from '@/components/actor/character/CharDetails.vue';
-export { default as CharTalents } from '@/components/actor/character/CharTalents.vue';
-export { default as CharEffects } from '@/components/actor/character/CharEffects.vue';
+export { default as CharSidebar } from "@/components/actor/character/CharSidebar.vue";
+export { default as CharHeader } from "@/components/actor/character/CharHeader.vue";
+export { default as CharDetails } from "@/components/actor/character/CharDetails.vue";
+export { default as CharTalents } from "@/components/actor/character/CharTalents.vue";
+export { default as CharEffects } from "@/components/actor/character/CharEffects.vue";
 // Items.
-export { default as ItemDescription } from '@/components/item/ItemDescription.vue';
-export { default as ItemHeader } from '@/components/item/ItemHeader.vue';
-export { default as ItemAttributes } from '@/components/item/ItemAttributes.vue';
-export { default as TalentResources } from '@/components/item/talent/TalentResources.vue';
+export { default as ItemDescription } from "@/components/item/ItemDescription.vue";
+export { default as ItemHeader } from "@/components/item/ItemHeader.vue";
+export { default as ItemAttributes } from "@/components/item/ItemAttributes.vue";
+export { default as TalentResources } from "@/components/item/talent/TalentResources.vue";
 // Parts.
-export { default as Tabs } from '@/components/parts/Tabs.vue';
-export { default as Tab } from '@/components/parts/Tab.vue';
-export { default as Prosemirror } from '@/components/parts/Prosemirror.vue';
+export { default as Tabs } from "@/components/parts/Tabs.vue";
+export { default as Tab } from "@/components/parts/Tab.vue";
+export { default as Prosemirror } from "@/components/parts/Prosemirror.vue";
