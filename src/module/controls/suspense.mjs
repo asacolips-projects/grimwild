119e32d8f49b3c0ecaa10b5659076cb1891c65c2
Matt Smith <mattsmith142003@gmail.com>
2025-02-26 18:54:19 -0600

    First pass at quick pools on scenes

--- src/module/controls/suspense.mjs
+++ src/module/controls/suspense.mjs
@@ -15,6 +15,51 @@ function setSuspense(value) {
 	game.settings.set("grimwild", "suspense", value);
 }
 
+function getScenePools() {
+	const scene = getScene();
+	if (!scene) return '';
+
+	const pools = scene.getFlag('grimwild', 'quickPools') ?? [];
+	const poolHtml = `
+	<div class="quick-pool-inner">
+		<div class="quick-pool-list">
+			${pools.map((pool, index) => `
+				<div class="quick-pool">
+					<div class="quick-pool-content">
+						<div class="quick-pool-current">
+							<span class="js-quick-pool-text" contentEditable="plaintext-only" data-pool="${index}" data-field="diceNum">${pool.diceNum}</span>d
+						</div>
+						${game.user.isGM ? `<button class="js-quick-pool-roll" type="button" data-roll-data="${pool.diceNum}" data-pool="${index}"><i class="fas fa-dice-d6"></i></button>` : ''}
+					</div>
+					<div class="quick-pool-label">
+						<span class="js-quick-pool-text" contentEditable="plaintext-only" data-pool="${index}" data-field="label">${pool.label}</span>
+						${game.user.isGM ? `<button class="js-quick-pool-delete" data-pool="${index}" type="button"><i class="fas fa-trash"></i></button>` : ''}
+					</div>
+				</div>
+			`).join('')}
+		</div>
+		<div class="quick-pool-adjust">
+			<button class="js-quick-pool-add">+ Pool</button>
+		</div>
+	</div>
+	`;
+	return poolHtml;
+}
+
+function addQuickPool() {
+	const scene = getScene();
+	if (!scene) return '';
+
+	const pools = scene.getFlag('grimwild', 'quickPools') ?? [];
+	pools.push({diceNum: 4, label: 'Label'});
+	scene.setFlag('grimwild', 'quickPools', pools);
+	ui.hotbar.render();
+}
+
+function getScene() {
+	return canvas?.scene ?? game.scenes.active;
+}
+
 class SuspenseTracker {
 	init() {
 		console.log("Suspense: initialising");
@@ -40,6 +85,8 @@ class SuspenseTracker {
 	render(value) {
 		const isGM = game.user.isGM;
 		const visibleToPlayers = game.settings.get("grimwild", "suspenseVisible");
+
+		console.log('CLASS RENDER', getScene());
 
 		let susControl = document.getElementById("sus-control");
 
@@ -69,13 +116,76 @@ class SuspenseTracker {
 			susControl.setAttribute("id", "sus-control");
 			document.getElementById("ui-bottom").prepend(susControl);
 		}
-		susControl.innerHTML = susControlInnerHTML;
+
+		const quickPoolHtml = getScenePools();
+		susControl.innerHTML = `${susControlInnerHTML}${quickPoolHtml}`;
 
 		if (isGM) {
 			document.getElementById("js-sus-up").onclick = () => setSuspense(getSuspense() + 1);
 			document.getElementById("js-sus-dn").onclick = () => {
 				setSuspense(Math.max(getSuspense() - 1, 0));
 			};
+			document.querySelector(".js-quick-pool-add").onclick = () => addQuickPool();
+
+			document.querySelectorAll(".js-quick-pool-delete").forEach(element => element.addEventListener('click', (event) => {
+				const { pool } = event.currentTarget.dataset;
+				const scene = getScene();
+				console.log('POOL', pool);
+				if (!scene) return;
+				const quickPools = scene.getFlag('grimwild', 'quickPools');
+				quickPools.splice(pool, 1);
+				scene.setFlag('grimwild', 'quickPools', quickPools);
+			}));
+
+			document.querySelectorAll('.js-quick-pool-text').forEach(element => element.addEventListener('focusout', (event) => {
+				const { pool, field } = event.currentTarget.dataset;
+				const scene = getScene();
+				if (!scene) return;
+				const quickPools = scene.getFlag('grimwild', 'quickPools');
+				let value = event.currentTarget.innerText;
+				// If value isn't a number on the pool field, exit early.
+				if (field === 'diceNum' && !Number.isNumeric(value)) {
+					this.render();
+					return;
+				}
+				quickPools[pool][field] = value;
+				scene.setFlag('grimwild', 'quickPools', quickPools);
+			}));
+
+			document.querySelectorAll('.js-quick-pool-roll').forEach(element => element.addEventListener('click', async (event) => {
+				let { rollData, pool } = event.currentTarget.dataset;
+				rollData = Number.isNumeric(rollData) ? Number(rollData) : 0;
+				const scene = getScene();
+				console.log('rollData', rollData, event.currentTarget);
+				if (!scene || !rollData) return;
+				const quickPools = scene.getFlag('grimwild', 'quickPools');
+
+				if (rollData) {
+					const roll = new grimwild.diePools(`{${rollData}d6}`, {});
+					const result = await roll.evaluate();
+					const dice = result.dice[0].results;
+					const dropped = dice.filter((die) => die.result < 4);
+					
+					const speaker = ChatMessage.getSpeaker();
+					const rollMode = game.settings.get("core", "rollMode");
+					const label = `[pool] ${event.target.closest('.quick-pool').querySelector('.quick-pool-label .js-quick-pool-text').innerText}`;
+					// Send to chat.
+					const msg = await roll.toMessage({
+						speaker: speaker,
+						rollMode: rollMode,
+						flavor: label
+					});
+
+					if (game.dice3d && msg?.id) {
+						await game.dice3d.waitFor3DAnimationByMessageID(msg.id);
+					}
+
+					rollData -= dropped.length;
+
+					quickPools[pool].diceNum = rollData;
+					scene.setFlag('grimwild', 'quickPools', quickPools);
+				}
+			}));
 		}
 		else if (!isNaN(value)) {
 			// flash the display to notify players that the suspense value changed
--- src/vue/components/actor/monster/MonsterBiography.vue
+++ src/vue/components/actor/monster/MonsterBiography.vue
@@ -5,6 +5,25 @@
 		<Prosemirror :editable="context.editable" :field="context.editors['system.biography']"/>
 	</fieldset>
 
+	<!-- Sights, Sounds, and Smells -->
+	<fieldset>
+		<legend>Sensories</legend>
+		<div class="form-group stacked">
+			<div class="form-group">
+				<label>Sights</label>
+				<input type="text" name="system.sensories.sights" v-model="context.system.sensories.sights">
+			</div>
+			<div class="form-group">
+				<label>Sounds</label>
+				<input type="text" name="system.sensories.sounds" v-model="context.system.sensories.sounds">
+			</div>
+			<div class="form-group">
+				<label>Smells</label>
+				<input type="text" name="system.sensories.smells" v-model="context.system.sensories.smells">
+			</div>
+		</div>
+	</fieldset>
+
 	<!-- Colors -->
 	<fieldset>
 		<legend>Colors</legend>
@@ -23,25 +42,6 @@
 				<div class="form-group"
 					v-html="context.customElements[`system.sensories.colors.${colorKey}.color`].outerHTML"
 				></div>
-			</div>
-		</div>
-	</fieldset>
-	
-	<!-- Sights, Sounds, and Smells -->
-	<fieldset>
-		<legend>Sensories</legend>
-		<div class="form-group stacked">
-			<div class="form-group">
-				<label>Sights</label>
-				<input type="text" name="system.sensories.sights" v-model="context.system.sensories.sights">
-			</div>
-			<div class="form-group">
-				<label>Sounds</label>
-				<input type="text" name="system.sensories.sounds" v-model="context.system.sensories.sounds">
-			</div>
-			<div class="form-group">
-				<label>Smells</label>
-				<input type="text" name="system.sensories.smells" v-model="context.system.sensories.smells">
 			</div>
 		</div>
 	</fieldset>
